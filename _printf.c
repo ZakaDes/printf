75f57a2835bb692c1c9b3f4fdee82a5a01a2be7a
ZakaDes <zakarialahmiyed@gmail.com>
2024-02-02 13:52:35 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,20 +1,14 @@
 #include "main.h"
-
 /**
  * _printf - a function that produces output according to a format
  * @format: the format
- *
  * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list arg;
 
-	int i, count, strc;
-
-	count = 0;
-	i = 0;
+	int i = 0, count = 0, strc;
 
 	va_start(arg, format);
 
@@ -24,15 +18,11 @@ int _printf(const char *format, ...)
 		{
 			putch(format[i]);
 		}
-
-
-
 		else if (format[i] == '%' && format[i + 1] == 'c')
 		{
 			putch(va_arg(arg, int));
 			i++;
 		}
-
 		else if (format[i + 1] == 's')
 		{
 			strc = put(va_arg(arg, char *));
@@ -40,15 +30,12 @@ int _printf(const char *format, ...)
 
 			count += (strc - 1);
 		}
-
 		else if (format[i + 1] == '%')
 		{
 			putch('%');
 		}
-
 		count += 1;
 	}
 va_end(arg);
 return (count);
-
 }
